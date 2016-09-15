ab8f4d7a67d6d6df171e2b8f81bf101de645f046
zdrossman <zachary.drossman@gmail.com>
2016-09-15 15:06:17 -0400

    update to be full screen, plus uses ngrok

--- app/src/main/java/com/leohealth/leo/WebActivity.java
+++ app/src/main/java/com/leohealth/leo/WebActivity.java
@@ -1,13 +1,20 @@
 package com.leohealth.leo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
-public class WebActivity extends AppCompatActivity {
+public class WebActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web);
+
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.loadUrl("http://leo-consumer-mobile.ngrok.io/docs");
     }
 }
+
